97474c306f9d4eb61bbc965e5ddbb967c46ebf0e
João Faria <joao.faria@unige.ch>
2024-03-13 11:46:10 +0100

    much fast nanpercentile

--- lbljf/core/math.py
+++ lbljf/core/math.py
@@ -306,6 +306,72 @@ def median(a: Union[list, np.ndarray],
         # return numpy function
         return np.median(a, axis=axis, **kwargs)
 
+# helper function for nanpercentile
+def _zvalue_from_index(arr, ind):
+    """
+    work around the limitation of np.choose() by employing np.take()
+        arr has to be a 3D array
+        ind has to be a 2D array containing values for z-indices to take from arr
+    See: http://stackoverflow.com/a/32091712/4169585
+    This is faster and more memory efficient than using the ogrid based solution with fancy indexing.
+    """
+    # get number of columns and rows
+    _, nC, nR = arr.shape
+    # get linear indices and extract elements with np.take()
+    idx = nC*nR*ind + np.arange(nC*nR).reshape((nC,nR))
+    return np.take(arr, idx)
+
+def nan_percentile(arr, q, axis=0):
+    if axis == 0:
+        arr = arr[None, :, :].copy()
+    elif axis == 1:
+        arr = arr[None, :, :].T.copy()
+    else:  # fall back
+        return np.nanpercentile(arr, q, axis)
+
+    # valid (non NaN) observations along the first axis
+    valid_obs = np.sum(np.isfinite(arr), axis=0)
+    # replace NaN with maximum
+    max_val = np.nanmax(arr)
+    nans = np.isnan(arr)
+    arr[nans] = max_val
+    # sort - former NaNs will move to the end
+    arr = np.sort(arr, axis=0)
+
+    # loop over requested quantiles
+    if isinstance(q, list):
+        qs = []
+        qs.extend(q)
+    else:
+        qs = [q]
+    if len(qs) < 2:
+        quant_arr = np.zeros(shape=(arr.shape[1], arr.shape[2]))
+    else:
+        quant_arr = np.zeros(shape=(len(qs), arr.shape[1], arr.shape[2]))
+
+    result = []
+    for i in range(len(qs)):
+        quant = qs[i]
+        # desired position as well as floor and ceiling of it
+        k_arr = (valid_obs - 1) * (quant / 100.0)
+        f_arr = np.floor(k_arr).astype(np.int32)
+        c_arr = np.ceil(k_arr).astype(np.int32)
+        fc_equal_k_mask = f_arr == c_arr
+
+        # linear interpolation (like numpy percentile) takes the fractional part of desired position
+        floor_val = _zvalue_from_index(arr=arr, ind=f_arr) * (c_arr - k_arr)
+        ceil_val = _zvalue_from_index(arr=arr, ind=c_arr) * (k_arr - f_arr)
+
+        quant_arr = floor_val + ceil_val
+        quant_arr[fc_equal_k_mask] = _zvalue_from_index(arr=arr, ind=k_arr.astype(np.int32))[fc_equal_k_mask]  # if floor == ceiling take floor value
+
+        result.append(quant_arr)
+
+    result = np.array(result).squeeze()
+    result[:, np.logical_and.reduce(nans.squeeze())] = np.nan
+
+    return result
+
 
 # =============================================================================
 # Numba JIT functionality
