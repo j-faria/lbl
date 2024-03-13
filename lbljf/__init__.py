a02431d4c11eccf01f592177597951aeb6791205
João Faria <joao.faria@unige.ch>
2024-03-13 10:53:14 +0100

    change name; no version requirements

---
+++ lbljf/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 2021-03-15
+
+@author: cook
+"""
+from lbl.core import base
+from lbl import recipes
+
+__NAME__ = 'lbl'
+__STRNAME__ = 'LBL'
+__version__ = base.__version__
+__date__ = base.__date__
+__authors__ = base.__authors__
+# clean recipe
+lbl_reset = recipes.lbl_reset.main
+# tellu-clean recipe
+lbl_telluclean = recipes.lbl_telluclean.main
+# alias to lbl template
+lbl_template = recipes.lbl_template.main
+# alias to lbl mask
+lbl_mask = recipes.lbl_mask.main
+# alias to lbl noise
+lbl_noise = recipes.lbl_noise.main
+# compute recipe
+lbl_compute = recipes.lbl_compute.main
+# compile recipe
+lbl_compil = recipes.lbl_compile.main
+# wrapper code
+lbl_wrap = recipes.lbl_wrap.main
+
+
+if __name__ == "__main__":
+    pass
